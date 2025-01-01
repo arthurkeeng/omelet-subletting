a63916ab3e6713fc4572a31ae58caa843fe6a03d
keeng arthur <121835767+arthurkeeng@users.noreply.github.com>
2025-01-01 17:18:09 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
  { rules: {
     "no-unused-vars": "off",
-    "no-console": "off"
+    "no-console": "off",
+   "@typescript-eslint/no-explicit-any": "off"
   }}
 ];
 
